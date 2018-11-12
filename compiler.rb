af3dfe83967d0134282d7914f35ca940546ffa9a
 TheSmartnik <misharinn@gmail.com>
2018-11-12 17:57:24 +0300

    Basic Parser

--- compiler.rb
+++ compiler.rb
@@ -7,6 +7,7 @@ class Tokenizer
     [:identifier, /\b[a-zA-Z]+\b/],
     [:integer, /\b[0-9]+\b/],
     [:oparen, /\(/],
+    [:comma, /,/],
     [:cparen, /\)/]
   ]
 
@@ -39,5 +40,104 @@ end
 
 Token = Struct.new(:type, :value)
 
+class Parser
+  def initialize(tokens)
+    @tokens = tokens
+  end
+
+  def parse
+    parse_def
+  end
+
+  def parse_def
+    consume(:def)
+    name = consume(:identifier).value
+    arg_names = parse_arg_names
+    body = parse_expr
+    consume(:end)
+
+    DefNode.new(name, arg_names, body)
+  end
+
+  def parse_arg_names
+    arg_names = []
+    consume(:oparen)
+    if peek(:identifier)
+      arg_names << consume(:identifier).value
+      while peek(:comma)
+        consume(:comma)
+        arg_names << consume(:identifier).value
+      end
+    end
+
+    consume(:cparen)
+    arg_names
+  end
+
+  def parse_expr
+    if peek(:integer)
+      parse_integer
+    elsif peek(:identifier) && peek(:oparen, 1)
+      parse_call
+    else
+      parse_var_ref
+    end
+  end
+
+  def parse_integer
+    IntegerNode.new(consume(:integer).value.to_i)
+  end
+
+  def parse_call
+    name = consume(:identifier)
+
+    arg_exprs = parse_arg_exprs
+
+    CallNode.new(name, arg_exprs)
+  end
+
+  def parse_var_ref
+    name = consume(:identifier).value
+
+    VarRefNode.new(name)
+  end
+
+  def parse_arg_exprs
+    arg_exprs = []
+    consume(:oparen)
+
+    if !peek(:cparen)
+      arg_exprs << parse_expr
+      while peek(:comma)
+        consume(:comma)
+        arg_exprs << parse_expr
+      end
+    end
+
+    consume(:cparen)
+    arg_exprs
+  end
+
+  def peek(type, offset = 0)
+    @tokens.fetch(offset).type == type
+  end
+
+  def consume(type)
+    token = @tokens.shift
+    if token.type == type
+      token
+    else
+      raise RuntimeError.new(
+        "Expected token type #{type}, but got #{token.type}"
+      )
+    end
+  end
+end
+
+DefNode = Struct.new(:name, :args, :body)
+IntegerNode = Struct.new(:value)
+CallNode = Struct.new(:name, :arg_exprs)
+VarRefNode = Struct.new(:name)
 tokens = Tokenizer.new(File.read("test.src")).tokenize
-puts tokens
+tree = Parser.new(tokens).parse
+puts tree
